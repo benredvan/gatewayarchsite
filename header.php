4161ea21cb1c4dd06ffecec7d64d681ba680ca94
Ben Smith <ben@redvanworkshop.com>
2017-12-14 15:01:37 -0600

    Wednesday work

--- header.php
+++ header.php
@@ -1,11 +1,74 @@
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
-<script>
-	$(function() {
-		$(".mobileMenu").on("click", function(){
-			$("header").toggleClass("show-nav");
+<html>
+<head>
+	<title>Gateway Arch - Dev Site</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g=" crossorigin="anonymous"></script>
+	<script>
+		$(function() { // event rigging
+
+			$(".mobileMenu").on("click", function(){
+				$("header").toggleClass("show-nav");
+			});
+			//debounced resize event
+			var resizeTimer;
+			$(window).on('resize', function(e) {
+				clearTimeout(resizeTimer);
+				resizeTimer = setTimeout(function() {
+				
+				//resizing has "stopped", cleanup menu
+				$("header").removeClass("show-nav");
+    			$("header li.activeMenu").removeClass("activeMenu");
+			            
+			  }, 250);
+			});
+			$(window).on('scroll', function(e){
+				if($(window).scrollTop() > 12){
+					$("body").addClass("pageScrolled");
+				}else{
+					$("body").removeClass("pageScrolled");
+				}
+			});
+
+			$(document).click(function(e) {
+	    		if ($(e.target).closest('header').length === 0) {
+	    			//close menu, cleanup
+	    			$("header").removeClass("show-nav");
+	    			$("header li.activeMenu").removeClass("activeMenu");
+	    		}
+	    	});
+	    	//level 2 menu show
+	    	/*
+	    	$("#navigation>li>a").on('touchstart', function(e){
+	    		e.preventDefault();
+
+	    		if($(this).parent().hasClass("activeMenu")){
+	    			//navigate
+	    			document.location=$(this).attr("href");
+	    		}else{
+		    		$(this).parent().addClass("activeMenu").siblings().removeClass("activeMenu");
+	    		}
+	    	});
+	    	*/
+
+	    	$("#navigation>li>a").on('touchstart', function (e) {
+			    e.preventDefault();
+	    		var rect = e.target.getBoundingClientRect();
+				var xpos = e.targetTouches[0].pageX - rect.left;
+	    		
+	    		if (xpos > ($(this).outerWidth() - 62)) { //62 pixels on right for icon click
+			        $(this).parent().toggleClass("activeMenu").siblings().removeClass("activeMenu");
+			    }else{
+	    			document.location=$(this).attr("href");
+			    }
+			});
 		});
-	});
-</script>
+	</script>
+</head>
+<body>
+
+
+
 <header>
 	<div class="innerWrapper">
 
@@ -24,9 +87,8 @@
 		</div>
 
 		<a href="/" class="homeLink" name="Gateway Arch Home"> </a>
-		<button id="buyTicketTop">Buy Tickets</button>
-		<ul id="navigation">
-			<li>
+		<button id="buyTicketTop"><label>Buy Tickets</label></button>
+		<ul id="navigation"><li>
 				<a href="/plan">Plan Your Visit</a>
 				<ul class="level-2">
 					<li><a href="/plan/page1">Hello Plan Your visit</a></li>
@@ -34,8 +96,7 @@
 					<li><a href="/plan/page3">Yes Page Three</a></li>
 					<li><a href="/plan/page4">Plan Visit Four</a></li>
 				</ul>
-			</li>
-			<li>
+			</li><li>
 				<a href="/experience">Experience</a>
 				<ul class="level-2">
 					<li><a href="/plan/page1">Hello Page Experience</a></li>
@@ -43,8 +104,7 @@
 					<li><a href="/plan/page3">Yes Page Three</a></li>
 					<li><a href="/plan/page4">Plan Visit Four</a></li>
 				</ul>
-			</li>
-			<li>
+			</li><li>
 				<a href="/groups">Groups</a>
 				<ul class="level-2">
 					<li><a href="/plan/page1">Hello it's Groups</a></li>
@@ -52,8 +112,7 @@
 					<li><a href="/plan/page3">Yes Page Three</a></li>
 					<li><a href="/plan/page4">Plan Visit Four</a></li>
 				</ul>
-			</li>
-			<li>
+			</li><li>
 				<a href="/events">Events</a>
 				<ul class="level-2">
 					<li><a href="/plan/page1">Hello Some Events</a></li>
@@ -61,7 +120,6 @@
 					<li><a href="/plan/page3">Yes Page Three</a></li>
 					<li><a href="/plan/page4">Plan Visit Four</a></li>
 				</ul>
-			</li>
-		</ul>
+			</li></ul>
 	</div>
 </header>
