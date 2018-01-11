2fd3d2cfc73f8311dafd169fdcb3b6d6759239cd
Ben Smith <ben@redvanworkshop.com>
2018-01-11 14:28:40 -0600

    Momentary update (alllmost still done)

--- js/app.js
+++ js/app.js
@@ -20,6 +20,102 @@ function checkScroll(){
 			.removeClass("activeMenu").siblings().removeClass("activeMenu");
 	}
 }
+function initAnimation(){
+	//-- animation
+    
+    var keys = [66,69,67,75,89,65,78,68,87,65,78,68,65,13];
+    var nextKey = 0;
+    $(window).keydown(function(e){
+        
+        var key = e.which;
+        if (key === keys[nextKey]) {
+            nextKey++;
+        }
+        else {
+            nextKey = 0;
+        }
+        
+        ColorMap();
+    });
+    
+    function ColorMap() {
+        var maxKeyIndex = keys.length - 1;
+        if(nextKey >= maxKeyIndex) {
+            $("body").append('<div class="a-container"><img class="a-container-b" src="http://gar.oldstlouis.com/wp-content/themes/gateway-arch/images/b.png" /></div>');
+        
+            $("body").append('<audio autoplay><source src="http://gar.oldstlouis.com/wp-content/themes/gateway-arch/images/harp.mp3" type="audio/mpeg"></audio>');
+            
+            $(".a-container").fadeIn(4000, function() {
+                animateDiv($('.a-container-b'));
+            });
+            
+            nextKey = 0;
+        }
+    }    
+    //go
+    
+    function makeNewPosition($target) {
+        
+        var $container = $target.parent();
+        
+        // Get viewport dimensions (remove the dimension of the div)
+        var h = $container.height() - $target.height();
+        var w = $container.width() - $target.width();
+    
+        var nh = Math.floor(Math.random() * h);
+        var nw = Math.floor(Math.random() * w);
+        var width = $target.width() * .7;
+        var height = $target.height() * .7;
+    
+        return [nh, nw, width, height];
+    
+    }
+    
+    function animateDiv($target) {
+        var newq = makeNewPosition($target);
+        var oldq = $target.offset();
+        var speed = calcSpeed([oldq.top, oldq.left], newq);
+        
+        if (newq[3] < 30) {
+            $target.animate({
+                top: newq[0],
+                left: newq[1],
+                width: newq[2],
+                height: newq[3],
+                opacity: 0,
+            }, speed, function() {
+                $(".a-container").remove();
+            });
+        }
+        else {
+            $target.animate({
+                top: newq[0],
+                left: newq[1],
+                width: newq[2],
+                height: newq[3],
+            }, speed, function() {
+                animateDiv($target);
+            });
+        }
+    };
+    
+    function calcSpeed(prev, next) {
+    
+        var x = Math.abs(prev[1] - next[1]);
+        var y = Math.abs(prev[0] - next[0]);
+    
+        var greatest = x > y ? x : y;
+    
+        var speedModifier = 0.17;
+    
+        var speed = Math.ceil(greatest / speedModifier);
+    
+        return speed;
+    
+    }
+
+    //-- end animation
+}
 
 $(function() { // event rigging
 
@@ -89,22 +185,39 @@ $(function() { // event rigging
 	var stickys = new Array();
 
 	var stickyHandler = function(direction){
-		
+
 		var previousWaypoint = this.waypoint.previous();
 		var nextWaypoint = this.waypoint.next();
+
+		var histElement = this.element; //the element we consult for history
 
 		$(".sticky-wrapper").removeClass('wp-current wp-next').addClass("wp-previous");
 		
 		$(this.element).parent(".sticky-wrapper").removeClass('wp-previous').addClass('wp-current');
 		
 		if(direction=="up"){
+
 			if(previousWaypoint){
+				histElement = previousWaypoint.element;
 				$(previousWaypoint.element).removeClass('wp-previous')//.addClass('wp-previous-upcoming');
+			}else{
+				histElement = null;
 			}
 		}else{
+			histElement = this.element;
 			if (nextWaypoint) {
 			  $(nextWaypoint.element).removeClass('wp-current').addClass('wp-next');
 			}
+		}
+
+		//history # of location
+
+		var theId =  $(histElement).closest(".sticky-wrapper").siblings("a").attr("id");
+		if(theId){
+			var theTitle = $(histElement).text();
+			history.replaceState(theId, theTitle, "#"+theId);
+		}else{
+			history.replaceState("", document.title, window.location.pathname + window.location.search);
 		}
 	}
 
@@ -122,6 +235,11 @@ $(function() { // event rigging
 			handler: stickyHandler
 		}))
 	})
+	$(".popper-opener").click(function(e) {
+        $(this).parent().toggleClass("open");
+    });
+
+	initAnimation();
 
 	//-- end event rigging
 });
--- tribe-events/modules/meta/venue.php
+++ tribe-events/modules/meta/venue.php
@@ -19,30 +19,37 @@ $website = tribe_get_venue_website_link();
 
 <div >
 	<img class="icon" src="http://gar.oldstlouis.com/wp-content/themes/gateway-arch/images/icon-map-pin.svg">
-	<div class="column_text">
+	<div class="column-text">
 		<h3 > Address </h3>
 		<?php do_action( 'tribe_events_single_meta_venue_section_start' ) ?>
 		<div>
-			<?php echo tribe_get_venue() ?><br />
-
+			<div>
+				<b><?php echo tribe_get_venue() ?></b>
+			</div>
 			<?php if ( tribe_address_exists() ) : ?>
-				<address class="tribe-events-address">
-					<?php echo tribe_get_full_address(); ?>
+				<div>
+					<address class="tribe-events-address">
+						<?php echo tribe_get_full_address(); ?>
 
-					<?php if ( tribe_show_google_map_link() ) : ?>
-						<?php echo tribe_get_map_link_html(); ?>
-					<?php endif; ?>
-				</address>
+						<?php if ( tribe_show_google_map_link() ) : ?>
+							<?php echo tribe_get_map_link_html(); ?>
+						<?php endif; ?>
+					</address>
+				</div>
 			<?php endif; ?>
 
 			<?php if ( ! empty( $phone ) ): ?>
-				<?php esc_html_e( 'Phone:', 'the-events-calendar' ) ?>
-				<?php echo $phone ?> <br />
+				<div>
+					<?php esc_html_e( 'Phone:', 'the-events-calendar' ) ?>
+					<?php echo $phone ?>
+				</div>
 			<?php endif ?>
 
 			<?php if ( ! empty( $website ) ): ?>
-				<?php esc_html_e( 'Website:', 'the-events-calendar' ) ?> 
-				<?php echo $website ?> <br />
+				<div>
+					<?php esc_html_e( 'Website:', 'the-events-calendar' ) ?> 
+					<?php echo $website ?>
+				</div>
 			<?php endif ?>
 		</div>
 		<?php do_action( 'tribe_events_single_meta_venue_section_end' ) ?>
